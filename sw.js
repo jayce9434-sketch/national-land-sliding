928fdd01c9e77e5efcdc9620df22650c9139318d
jayce9434-sketch <jayce9434@gmail.com>
2026-08-18 11:15:05 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE="nls-v1";const ASSETS=["./","index.html","style.css","app.js","config.js","manifest.webmanifest","icons/icon-192.png","icons/icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
